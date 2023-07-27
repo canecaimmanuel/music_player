c42fd07eca99f461f0bccf4da79b32ea05ef0e7c
immanuel caneca <immancaneca@gmail.com>
2023-07-27 23:31:33 +0800

    added and create, fetch and design

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name : "My Heroine - Discovering Waterfront",
+        artist : "Silverstein",
+        img : "myheroine",
+        audio : "myheroine"
+    },
+    {
+        name : "Baby - Bieber ft. Ludacris",
+        artist : "Justine Bieber",
+        img : "babycover",
+        audio : "baby"
+    },
+    {
+        name : "Only One",
+        artist : "Yellowcard",
+        img : "onlyone",
+        audio : "onlyone"
+    },
+    {
+        name : "Despacito - Luis ft. Daddy Fonsi",
+        artist : "Luis Fonsi",
+        img : "despacito",
+        audio : "despacito"
+    },
+]
